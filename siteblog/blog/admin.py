d667f0908a39c9681fc6e19cdf0bd58de456b49d
Nikita <developer.rni@gmail.com>
2021-04-16 19:09:07 +0300

    installed ckeditor

--- siteblog/blog/admin.py
+++ siteblog/blog/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 from .models import *
 
 
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    form = PostAdminForm
 
 
 class CategoryAdmin(admin.ModelAdmin):
